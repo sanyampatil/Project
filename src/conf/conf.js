97a11c755ded350ade322e4455bce1f0647a1ce3
sanyam <sanyampatil869@gmail.com>
2023-10-16 23:41:16 +0530

    first commit

---
+++ src/conf/conf.js
@@ -0,0 +1,12 @@
+const consf = {
+    appWriteUrl:String(import.meta.env.VITE_APPWRITE_WRITE),
+    appWriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appWriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appWriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appWriteBukectId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+
+
+
+
+
+}
