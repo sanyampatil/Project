2e481920ad5abcd778232b5350ede44aa55e9fd7
sanyam <patilsanyam869@gmail.com>
2023-12-22 12:55:03 +0530

    initial commit on app configure

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,62 +1,62 @@
-import conf from '../conf.js'
-import {Client,Account,ID } from 'appwrite'
- export class AuthService{
+import conf from '../conf/conf.js';
+// import conf from './config'
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService {
     client = new Client();
     account;
-    constructor(){
+
+    constructor() {
         this.client
-         .setEndpoint(conf.appwriteUrl)
-        .setProject(conf.appwriteProjectId);
-        this.account = new  Account(this.client);
-
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+            
     }
-    async createAccount({email,password,name}){
-        try{
-           const userAccount= await this.account.create(email,password,name);
-           if(userAccount){
-            // call another method
-            return this.login({email,password})
 
-           }else{
-            return userAccount
-           }
-
-        }
-        catch(error){
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // call another method
+                return this.login({email, password});
+            } else {
+               return  userAccount;
+            }
+        } catch (error) {
             throw error;
         }
     }
-    async login({email,password}){
-       try{
-        return await this.account.createEmailSession(email,password);
-   
-       }
-       catch(error){
-        throw error;
-       }
-   
-    }
-    async getCurrentUser(){
-        try{
-            return await this.account,get();
 
-
-        }catch(error){
-            console.log("apwrite serive::getCurrentuser::error",error)
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
         }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+
         return null;
     }
 
+    async logout() {
 
-    async  logout(){
-        try{
+        try {
             await this.account.deleteSessions();
-        } catch(error){
-            console.log("Appwrite serive :: logout ::error ",error)
+        } catch (error) {
+            console.log("Appwrite serive :: logout :: error", error);
         }
     }
- }
+}
 
+const authService = new AuthService();
 
- const AuthService = new AuthService();
- export default AuthService
+export default authService;
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,8 +1,9 @@
 const conf = {
-    appWriteUrl:String(import.meta.env.VITE_APPWRITE_WRITE),
-    appWriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appWriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appWriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appWriteBukectId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 }
+
+export default conf
