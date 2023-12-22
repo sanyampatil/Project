b90da0036f81e6c83789045647bbb793f1d8baaf
sanyam <patilsanyam869@gmail.com>
2023-12-22 10:34:16 +0530

    start to create components

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit';
-import authSlice from './authSlice';
 
 const store = configureStore({
-    reducer: {
-        // auth : authSlice,
-        // //TODO: add more slices here for posts
-    }
+    reducer: {}
 });
 
 
