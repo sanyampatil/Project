9de34758a4dc87e710768857987a03ae04b8d876
sanyam <patilsanyam869@gmail.com>
2023-12-25 17:52:58 +0530

    start Signup form

---
+++ src/components/Signup.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+import authService from '../appwrite/auth'
+import {Link ,useNavigate} from 'react-router-dom'
+import {login} from '../store/authSlice'
+import {Button, Input, Logo} from './index.js'
+import {useDispatch} from 'react-redux'
+import {useForm} from 'react-hook-form'
+
+function Signup() {
+    const navigate = useNavigate()
+    const [error, setError] = useState("")
+    const dispatch = useDispatch()
+    const {register, handleSubmit} = useForm()
+
+    const create = async(data) => {
+        setError("")
+        try {
+            const userData = await authService.createAccount(data)
+            if (userData) {
+                const userData = await authService.getCurrentUser()
+                if(userData) dispatch(login(userData));
+                navigate("/")
+            }
+        } catch (error) {
+            setError(error.message)
+        }
+    }
+
+  return (
+   <h1>Signup</h1>
+   
+  )
+}
+
+export default Signup
