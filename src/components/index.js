caad1384c9da305ce5cd5fd21e6d4ddcd194ce45
sanyam <patilsanyam869@gmail.com>
2023-12-27 12:42:14 +0530

    start post-form

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,15 @@ import {Header} from './Header/Header'
 import Container from './container/Container'
 import Logo from './Logo'
 import LogoutBtn from './Header/LogoutBtn'
+import {RTE} from './RTE'
+
 export{
  Footer,
  Header,
  Container,
  Logo,
- LogoutBtn
+ LogoutBtn,
+ RTE
+ 
 }
 
